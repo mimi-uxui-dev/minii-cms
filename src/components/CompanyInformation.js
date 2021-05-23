e5f389b34a723ee87ac18aa1be9353174a5a295f
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-05-23 11:48:28 +0100

    update

--- src/components/CompanyInformation.js
+++ src/components/CompanyInformation.js
@@ -7,6 +7,8 @@ import cookies from 'js-cookie'
 import spinner from '../assets/img/spinner.svg'
 import parse from 'html-react-parser'
 
+
+
 import lineGreenish from '../assets/img/Vector 5.svg'
 import quotes from '../assets/img/quotes.svg'
 import menImg from '../assets/img/CompanyInfo/men01.jpg'
