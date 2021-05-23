f0040d0fdf04de19f91d238e72ecb1c19f579d75
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-05-23 13:57:43 +0100

    WWO

--- src/components/WhatWeOffer.js
+++ src/components/WhatWeOffer.js
@@ -37,7 +37,7 @@ function WhatWeOffer({ lang }) {
 
                   <div className='wwo2Grid' >
 
-                        {
+                        {/*  {
                               services.map(s => <div className='wwo2Grid_'>
                                     <img className='wwo2Grid_Img' src={s.photo} alt="" />
                                     <p className='wwo2Grid_title'>{cookies.get('i18next') === 'ar' ? s.name__ar : (cookies.get('i18next') == 'fr' ? s.name__fr : s.name__en)}</p>
@@ -45,8 +45,49 @@ function WhatWeOffer({ lang }) {
                                     <p className='wwo2Grid_desc'> {cookies.get('i18next') === 'ar' ? parse(`${s.description__ar}`) : (cookies.get('i18next') == 'fr' ? parse(`${s.description__fr}`) : parse(`${s.description__en}`))}  </p>
                               </div>)
                         }
+                        */}
 
+                        <div className='wwo2Grid_'>
+                              <img className='wwo2Grid_Img' src='https://i.ibb.co/YbrpQFk/Group-26.jpg' alt="" />
+                              <p className='wwo2Grid_title'>Organic Stuff</p>
+                              <img src={lineGreenish} alt="" />
+                              <p className='wwo2Grid_desc'>  Sed suscipit quis ornare sem molestie. Sed suscipit sollicitudin nullantum odio. Quisque ullamcorper, suscipit sollicitudin nullantum odio </p>
+                        </div>
 
+                        <div className='wwo2Grid_'>
+                              <img className='wwo2Grid_Img' src='https://i.ibb.co/ctP7rbp/Group-27.jpg' alt="" />
+                              <p className='wwo2Grid_title'>Organic Stuff</p>
+                              <img src={lineGreenish} alt="" />
+                              <p className='wwo2Grid_desc'>  Sed suscipit quis ornare sem molestie. Sed suscipit sollicitudin nullantum odio. Quisque ullamcorper, suscipit sollicitudin nullantum odio </p>
+                        </div>
+
+                        <div className='wwo2Grid_'>
+                              <img className='wwo2Grid_Img' src='https://i.ibb.co/QQZ1S1s/Group-28.jpg' alt="" />
+                              <p className='wwo2Grid_title'>Organic Stuff</p>
+                              <img src={lineGreenish} alt="" />
+                              <p className='wwo2Grid_desc'>  Sed suscipit quis ornare sem molestie. Sed suscipit sollicitudin nullantum odio. Quisque ullamcorper, suscipit sollicitudin nullantum odio </p>
+                        </div>
+
+                        <div className='wwo2Grid_'>
+                              <img className='wwo2Grid_Img' src='https://i.ibb.co/VVdbtdw/Group-29.jpg' alt="" />
+                              <p className='wwo2Grid_title'>Organic Stuff</p>
+                              <img src={lineGreenish} alt="" />
+                              <p className='wwo2Grid_desc'>  Sed suscipit quis ornare sem molestie. Sed suscipit sollicitudin nullantum odio. Quisque ullamcorper, suscipit sollicitudin nullantum odio </p>
+                        </div>
+
+                        <div className='wwo2Grid_'>
+                              <img className='wwo2Grid_Img' src='https://i.ibb.co/wd1qrZk/Group-30.jpg' alt="" />
+                              <p className='wwo2Grid_title'>Organic Stuff</p>
+                              <img src={lineGreenish} alt="" />
+                              <p className='wwo2Grid_desc'>  Sed suscipit quis ornare sem molestie. Sed suscipit sollicitudin nullantum odio. Quisque ullamcorper, suscipit sollicitudin nullantum odio </p>
+                        </div>
+
+                        <div className='wwo2Grid_'>
+                              <img className='wwo2Grid_Img' src='https://i.ibb.co/8gjdQVv/Group-31.jpg' alt="" />
+                              <p className='wwo2Grid_title'>Organic Stuff</p>
+                              <img src={lineGreenish} alt="" />
+                              <p className='wwo2Grid_desc'>  Sed suscipit quis ornare sem molestie. Sed suscipit sollicitudin nullantum odio. Quisque ullamcorper, suscipit sollicitudin nullantum odio </p>
+                        </div>
 
                   </div>
             </div>
