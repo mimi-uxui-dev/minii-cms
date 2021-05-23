a5509b8ccbff68abbea2973e5f5ba5d7173c672b
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-05-23 13:31:27 +0100

    132

--- src/components/SliderComponent.js
+++ src/components/SliderComponent.js
@@ -35,6 +35,10 @@ function SliderComponent() {
 
      }, [])
 
+     const imgs = [
+          'https://i.ibb.co/z2W7BZv/02.jpg',
+          'https://i.ibb.co/ZcvrxQv/01.jpg'
+     ]
 
      const phrases = ['Live healthy', 'New This Week', 'Green Life, Best Life']
 
@@ -51,6 +55,7 @@ function SliderComponent() {
                                    sliders.map(slider =>
                                         <div key={slider.id} className="section">
                                              <img src={slider.photo} alt="" style={{ maxWidth: '100%', width: '100%', height: '-webkit-fill-available', objectFit: 'cover' }} />
+
                                              <div className='sliderTxt '>
                                                   <p className='sliderAboveHeader' >{phrases[Math.floor(Math.random() * phrases.length)]}</p>
                                                   <p className="sliderHeader">{cookies.get('i18next') === 'ar' ? slider.title__ar : (cookies.get('i18next') == 'fr' ? slider.title__fr : slider.title__en)} </p>
@@ -60,6 +65,30 @@ function SliderComponent() {
                                         </div>
                                    )
                               }
+
+
+                              {/* <div key={slider.id} className="section">
+                                   <img src='https://i.ibb.co/T0Db7t3/02.jpg' alt="" style={{ maxWidth: '100%', width: '100%', height: '-webkit-fill-available', objectFit: 'cover' }} />
+
+                                   <div className='sliderTxt '>
+                                        <p className='sliderAboveHeader' >Live Healthy</p>
+                                        <p className="sliderHeader">{cookies.get('i18next') === 'ar' ? slider.title__ar : (cookies.get('i18next') == 'fr' ? slider.title__fr : slider.title__en)} </p>
+                                        <p className="sliderContent" dangerouslySetInnerHTML={{ __html: cookies.get('i18next') === 'ar' ? slider.description__ar : (cookies.get('i18next') == 'fr' ? slider.description__fr : slider.description__en) }} ></p>
+                                        <Link className='sliderCTA' to={'/products'}>Shop now »</Link>
+                                   </div>
+                              </div>
+
+                              <div key={slider.id} className="section">
+                                   <img src='https://i.ibb.co/T0Db7t3/02.jpg' alt="" style={{ maxWidth: '100%', width: '100%', height: '-webkit-fill-available', objectFit: 'cover' }} />
+
+                                   <div className='sliderTxt '>
+                                        <p className='sliderAboveHeader' >{phrases[Math.floor(Math.random() * phrases.length)]}</p>
+                                        <p className="sliderHeader">{cookies.get('i18next') === 'ar' ? slider.title__ar : (cookies.get('i18next') == 'fr' ? slider.title__fr : slider.title__en)} </p>
+                                        <p className="sliderContent" dangerouslySetInnerHTML={{ __html: cookies.get('i18next') === 'ar' ? slider.description__ar : (cookies.get('i18next') == 'fr' ? slider.description__fr : slider.description__en) }} ></p>
+                                        <Link className='sliderCTA' to={'/products'}>Shop now »</Link>
+                                   </div>
+                              </div> */}
+
                          </Slider>
                }
           </div>
